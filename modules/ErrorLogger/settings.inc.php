199d7d7bd6e05f67ea75dda07d094ca78ac28f48
T.Binh-PrimeLabo <thanh.binh@primelabo.com.vn>
2019-10-28 00:51:36 +0700

    CMS2212.2.Log

--- modules/ErrorLogger/settings.inc.php
+++ modules/ErrorLogger/settings.inc.php
@@ -1,5 +1,5 @@
 <?php
-//saved at 2019-10-19 15:40:16
+//saved at 2019-10-28 00:45:33
 define('MOD_ERROR_LOGGER_INFO',           false);
 define('MOD_ERROR_LOGGER_WARNING',        false);
 define('MOD_ERROR_LOGGER_ERROR',          true);
